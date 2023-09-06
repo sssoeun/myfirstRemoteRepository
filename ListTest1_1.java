7b1a9d1073b146999fd0f440f1772783662789a8
colacider97 <colacider97@gmail.com>
2023-09-06 17:38:47 +0900

    변경됨_주석_추가

--- ListTest1_1.java
+++ ListTest1_1.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 public class ListTest1_1 {
 
 	public static void main(String[] args) {
+
+
+		// list objects created
 		ArrayList<Double> arrayList = new ArrayList<Double>();
 		
 		System.out.println(arrayList);
@@ -13,6 +16,7 @@ public class ListTest1_1 {
 		arrayList.add(20.0);
 		arrayList.add(30.0);
 		
+		// arrayList print
 		System.out.println(arrayList);
 		
 		double tot = 0;
